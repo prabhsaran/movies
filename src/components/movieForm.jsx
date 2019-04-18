597f120c21a29b70acaa686bf28493ff988aa7e0
Prabhvir Saran <acsio@Prabhvirs-MacBook-Pro.local>
2019-04-18 15:46:39 -0700

    added routing to movies form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const MovieForm = () => {
-    return <h1>Movie Form</h1>;
-}
- 
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button
+        onClick={() => history.push("/movies")}
+        className="btn-btn-primary"
+      >
+        Save
+      </button>
+    </div>
+  );
+};
+
 export default MovieForm;
